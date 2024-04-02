58c7e94e83fc328556b239d0bb7077a1485622f5
Madhav Gupta <e22cseu0827@bennett.edu.in>
2024-04-02 16:43:36 +0530

    Oauth Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:water_saver/firebase_options.dart';
+import 'package:water_saver/screens/homepage.dart';
 import 'package:water_saver/screens/otp_screen.dart';
+import 'package:water_saver/screens/password_registration.dart';
 import 'package:water_saver/screens/signup_screen.dart';
 
 void main() async {
@@ -9,19 +13,32 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  runApp(const MainApp());
+  bool loggedIn = false;
+  User? user = await FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    loggedIn = true;
+  }
+  runApp(MainApp(
+    loggedIn: loggedIn,
+    user: user,
+  ));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required this.loggedIn, required this.user});
+  final bool loggedIn;
+  final User? user;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: SignUpScreen(),
+      home: loggedIn
+          ? HomeScreen(
+              user: user,
+            )
+          : SignUpScreen(),
     );
   }
 }
