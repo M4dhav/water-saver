0e6974cb831c37202603578464818e6531637034
Mayank4352 <120477383+Mayank4352@users.noreply.github.com>
2025-06-14 17:28:15 +0530

    feat: updated login logic and reservoir bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:water_saver/firebase_options.dart';
@@ -9,6 +11,10 @@ import 'package:water_saver/screens/login_screen_1.dart';
 import 'package:water_saver/screens/splash.dart';
 import 'package:water_saver/widgets/nav_bar.dart';
 import 'package:go_router/go_router.dart';
+import 'package:water_saver/screens/onboarding_screen_1.dart';
+import 'package:water_saver/screens/onboarding_screen_2.dart';
+import 'package:water_saver/screens/onboarding_screen_3.dart';
+import 'package:water_saver/screens/onboarding_screen_4.dart';
 
 final sharedPreferencesProvider = Provider<SharedPreferencesAsync>((ref) {
   return SharedPreferencesAsync();
@@ -17,16 +23,21 @@ final sharedPreferencesProvider = Provider<SharedPreferencesAsync>((ref) {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final prefs = await SharedPreferences.getInstance();
+  Get.put(prefs);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   bool loggedIn = false;
+  bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
   User? user = FirebaseAuth.instance.currentUser;
   if (user != null) {
     loggedIn = true;
   }
   runApp(MainApp(
     loggedIn: loggedIn,
+    isFirstTime: isFirstTime,
   ));
 }
 
@@ -36,12 +47,13 @@ class MainApp extends StatelessWidget {
   MainApp({
     super.key,
     required this.loggedIn,
+    required this.isFirstTime,
   }) : _router = GoRouter(
+          initialLocation: '/splash',
           routes: [
             GoRoute(
-              path: '/',
-              builder: (context, state) =>
-                  loggedIn ? const BottomNavBar() : const SplashScreen(),
+              path: '/splash',
+              builder: (context, state) => const SplashScreen(),
             ),
             GoRoute(
               path: '/login',
@@ -51,18 +63,35 @@ class MainApp extends StatelessWidget {
               path: '/home',
               builder: (context, state) => const BottomNavBar(),
             ),
+            GoRoute(
+              path: '/onboarding1',
+              builder: (context, state) => const OnboardingScreen1(),
+            ),
+            GoRoute(
+              path: '/onboarding2',
+              builder: (context, state) => const OnboardingScreen2(),
+            ),
+            GoRoute(
+              path: '/onboarding3',
+              builder: (context, state) => const OnboardingScreen3(),
+            ),
+            GoRoute(
+              path: '/onboarding4',
+              builder: (context, state) => const OnboardingScreen4(),
+            ),
           ],
         );
 
   final bool loggedIn;
+  final bool isFirstTime;
 
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
       child: ResponsiveSizer(builder: (context, orientation, screenType) {
         return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
           routerConfig: _router,
+          debugShowCheckedModeBanner: false,
         );
       }),
     );
